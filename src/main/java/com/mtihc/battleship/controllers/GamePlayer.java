5ffc231ccc7b0859fe2e65ecd1f9c41883850316
Mitch <mitchstoffels@gmail.com>
2013-03-27 00:07:56 +0100

    Cleanup of GamePlayer class

--- src/main/java/com/mtihc/battleship/controllers/GamePlayer.java
+++ src/main/java/com/mtihc/battleship/controllers/GamePlayer.java
@@ -4,6 +4,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.Event.Result;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -41,68 +42,80 @@ public class GamePlayer {
 	}
 	
 	protected void onPlayerInteract(PlayerInteractEvent event) {
+
+		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			if(controller.areAllShipsPlaced(view.getBoard())) {
+				placeShip(event);
+			}
+		}
+		else if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+			
+		}
+		else {
+			return;
+		}
+		
 		event.setUseInteractedBlock(Result.DENY);
 		event.setUseItemInHand(Result.DENY);
 		event.setCancelled(true);
 		
-		if(!controller.areAllShipsPlaced(view.getBoard())) {
-			// still in setup mode, because not all ships are placed
-			
-			BlockFace facing = BoardView.yawToFace(event.getPlayer().getLocation().getYaw());
-			Board board = view.getBoard();
-			BoardView boardView = view.getInteractiveView();
-			Tile tile = boardView.getLocationStrategy().locationToTile(boardView, event.getClickedBlock().getLocation());
-			if(tile == null) {
-				// clicked outside of the board
-				return;
-			}
-			if(tile.hasShip()) {
-				// there's already a ship
-				return;
-			}
-			
-			Ship[] ships = board.getShips();
-			for (int i = 0; i < ships.length; i++) {
-				Ship ship = ships[i];
-				
-				if(ship.getType().isSimilar(event.getItem()) && !ship.isPlaced()) {
-					// placing a ship
-					
-					if(!placeShip(ship, tile, event.getClickedBlock(), facing)) {
-						// couldn't place this ship
-						// continue to try the next ship
-						continue;
-					}
-					
-					
-					// remove ship items from player's inventory
-					int amountToRemove = ship.getSize();
-					ItemStack itemToRemove = ship.getType().getNormal().toItemStack();
-					for (ItemStack item : event.getPlayer().getInventory()) {
-						if(itemToRemove.isSimilar(item)) {
-							if (item.getAmount() > amountToRemove){
-								item.setAmount(item.getAmount() - amountToRemove);
-			                    amountToRemove = 0;
-			                }else{
-			                    amountToRemove -= item.getAmount();
-			                    event.getPlayer().getInventory().remove(item);
-			                }
-						}
-					}
-					// TODO event.getPlayer().updateInventory();
-					
-					// break the loop, since we placed a ship
-					break;
-				}
-			}
-		}
-		else {
-			// not in setup mode, must be battling
-			
-			// TODO place TNT
-		}
 	}
 	
+	private void placeShip(PlayerInteractEvent event) {
+		// still in setup mode, because not all ships are placed
+		BlockFace facing = BoardView.yawToFace(event.getPlayer().getLocation().getYaw());
+		Board board = view.getBoard();
+		BoardView boardView = view.getInteractiveView();
+		Tile tile = boardView.getLocationStrategy().locationToTile(boardView, event.getClickedBlock().getLocation());
+		if(tile == null) {
+			// clicked outside of the board
+			return;
+		}
+		if(tile.hasShip()) {
+			// there's already a ship
+			return;
+		}
+		
+		Ship[] ships = board.getShips();
+		for (int i = 0; i < ships.length; i++) {
+			Ship ship = ships[i];
+			
+			if(!ship.getType().isSimilar(event.getItem()) || ship.isPlaced()) {
+				// different type of ship, or it's already placed,
+				// continue to try the next ship
+				continue;
+			}
+			// placing a ship
+			
+
+			if(!placeShip(ship, tile, event.getClickedBlock(), facing)) {
+				// couldn't place this ship
+				// continue to try the next ship
+				continue;
+			}
+			
+			
+			// remove ship items from player's inventory
+			int amountToRemove = ship.getSize();
+			ItemStack itemToRemove = ship.getType().getNormal().toItemStack();
+			for (ItemStack item : event.getPlayer().getInventory()) {
+				if(itemToRemove.isSimilar(item)) {
+					if (item.getAmount() > amountToRemove){
+						item.setAmount(item.getAmount() - amountToRemove);
+	                    amountToRemove = 0;
+	                }else{
+	                    amountToRemove -= item.getAmount();
+	                    event.getPlayer().getInventory().remove(item);
+	                }
+				}
+			}
+			// TODO event.getPlayer().updateInventory();
+			
+			// break the loop, since we placed a ship
+			break;
+		}
+	}
+
 	private boolean placeShip(Ship ship, Tile clickedTile, Block clickedBlock, BlockFace facing) {
 		BoardView boardView = view.getInteractiveView();
 		Tile[] shipTiles = new Tile[ship.getSize()];
